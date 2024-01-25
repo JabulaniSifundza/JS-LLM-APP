e080b51d728af60f537f754d03adf61a6ee5c719
JabulaniSifundza <jjsifundza@gmail.com>
2024-01-25 14:58:56 +0200

    Embedding function Native to Google

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const { v4 } = require("uuid");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenerativeAI, TaskType } = require("@google/generative-ai");
 const { chroma } = require("chromadb");
 var admin = require("firebase-admin");
 const express = require("express");
@@ -10,21 +10,25 @@ const app = express();
 app.use(express.static("public"));
 const serviceAccount = require("./service-account.json");
 const { PDFDocument } = require("pdf-lib");
-//const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
-
+const { Model } = require("firebase-admin/lib/machine-learning/machine-learning");
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "embedding-001" });
 const port = 3000;
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 const storage = admin.storage();
+
 async function extractPdfText(url) {
     try{
-        const response = await axios.get(url, { responseType: "arraybuffer" });
-        const data = await pdfParse(response.data);
-        return data.text;
+      const response = await axios.get(url, { responseType: "arraybuffer" });
+      const data = await pdfParse(response.data);
+      return data.text;
     }
     catch(error){
-        console.error(error)
+      console.error(error)
     }
 }
 
@@ -36,34 +40,87 @@ function formatArrayElements(arr) {
   return formattedElements;
 }
 
-app.get("/home", async (req, res) => {
-  const finance_books = [
-    "https://firebasestorage.googleapis.com/v0/b/financial-advisor-llm.appspot.com/o/LLM%20Sources%2FInvestopedia.pdf?alt=media&token=ecd64c82-f518-4592-80e5-be6a73543998",
-    "https://firebasestorage.googleapis.com/v0/b/financial-advisor-llm.appspot.com/o/LLM%20Sources%2FDean%20Paxson%2C%20Douglas%20Wood%2C%20Blackwell%20Encyclopedic%20Dictionary%20of%20Finance%20(1997).pdf?alt=media&token=55ad60d9-8fd1-44b8-88fc-63d5076781be",
-    "https://firebasestorage.googleapis.com/v0/b/financial-advisor-llm.appspot.com/o/LLM%20Sources%2FA-plain-English-guide-to-financial-terms.pdf?alt=media&token=ea04f56f-9e42-43f3-be2e-61d62c516574",
-  ];
-  const knowledge = []
-  const cc = new chroma.ChromaClient({ path: "http://localhost:8000" });
-  await cc.reset();
+async function embedRetrievalDocs(texts){
+  const result = await model.batchEmbedContents({
+    requests: texts.map((text) => ({
+      content: { parts: [{ text: text }] },
+      taskType: TaskType.RETRIEVAL_DOCUMENT,
+    })),
+  });
+  const embeddings = result.embeddings;
+  return embeddings.map((embedding, index)=>({text: texts[index], values: embedding.values}))
+}
 
-  for(const book of finance_books){
-    const book_text = await extractPdfText(book)
-    knowledge.push(book_text);
+async function embedRetrievalQuery(query){
+  const result = await model.embedContent({
+    content: { parts: [{ text: query }] },
+    taskType: TaskType.RETRIEVAL_QUERY,
+  })
+  const embedding = result.embedding;
+  return embedding.values;
+}
+
+// Returns Euclidean Distance between 2 vectors
+// AKA Measures distance between vectors for similarity
+function euclideanDistance(a, b){
+  let sum = 0
+  for(let n = 0; n < a.length; n++){
+    sum += Math.pow(a[n] - b[n], 2)
   }
-  const google = new chroma.GoogleGenerativeAiEmbeddingFunction({
-    googleApiKey: "<APIKEY>",
-  });
-  const collection = await cc.createCollection({
-    name: "financial-info",
-    embeddingFunction: google,
-  });
+  return Math.sqrt(sum)
+}
 
-  await collection.add({
-    ids: formatArrayElements(knowledge),
-    documents: knowledge,
-  });
+async function performQuerySearch(query, docs){
+  const queryValues = await embedRetrievalQuery(query)
+  console.log(query)
+  for(const doc of docs){
+    console.log(
+      " ",
+      euclideanDistance(doc.values, queryValues),
+      doc.text.substr(0, 40)
+    )
+    return (" ", euclideanDistance(doc.values, queryValues), doc.text.substr(0, 40));
+  }
+}
 
-  res.sendFile(__dirname + "/public/index.html");
+async function runEmbed(question){
+  const docs = await embedRetrievalDocs([])
+  await performQuerySearch(question, docs);
+}
+
+app.get("/home", async (req, res) => {
+  try{
+    const finance_books = [
+      "https://firebasestorage.googleapis.com/v0/b/financial-advisor-llm.appspot.com/o/LLM%20Sources%2FInvestopedia.pdf?alt=media&token=ecd64c82-f518-4592-80e5-be6a73543998",
+      "https://firebasestorage.googleapis.com/v0/b/financial-advisor-llm.appspot.com/o/LLM%20Sources%2FDean%20Paxson%2C%20Douglas%20Wood%2C%20Blackwell%20Encyclopedic%20Dictionary%20of%20Finance%20(1997).pdf?alt=media&token=55ad60d9-8fd1-44b8-88fc-63d5076781be",
+      "https://firebasestorage.googleapis.com/v0/b/financial-advisor-llm.appspot.com/o/LLM%20Sources%2FA-plain-English-guide-to-financial-terms.pdf?alt=media&token=ea04f56f-9e42-43f3-be2e-61d62c516574",
+    ];
+    const knowledge = [];
+    const cc = new chroma.ChromaClient({ path: "http://localhost:8000" });
+    await cc.reset();
+
+    for (const book of finance_books) {
+      const book_text = await extractPdfText(book);
+      knowledge.push(book_text);
+    }
+    const google = new chroma.GoogleGenerativeAiEmbeddingFunction({
+      googleApiKey: GEMINI_API_KEY,
+    });
+    const collection = await cc.createCollection({
+      name: "financial-info",
+      embeddingFunction: google,
+    });
+
+    await collection.add({
+      ids: formatArrayElements(knowledge),
+      documents: knowledge,
+    });
+    res.sendFile(__dirname + "/public/index.html");
+
+  }
+  catch(error){
+    console.error(error)
+  }
 });
 
 app.get("/", (req, res) => {
